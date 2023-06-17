2b411d177d14dc97ac95f123283857fa9b946116
Xavier Das <xav0anime@gmail.com>
2023-06-17 22:12:50 +0530

    some text added by xavier

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -25,14 +25,12 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <h1 className="text-2xl font-bold underline">
-        tailwind says hello! x2
-      </h1>
+      <h1 className="text-2xl font-bold underline">tailwind says hello! x2</h1>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more and more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
