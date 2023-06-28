03ca556d62f154dc11313e573b4787b3e5c1d20c
xav1994das <xav0anime@gmail.com>
2023-06-28 23:08:19 +0530

    created few components for the front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Signup from "./pages/SignUp";
 import { Routes, Route } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import Dashboard from "./pages/Dashboard";
+import AddRestaurant from "./pages/AddRestaurant";
 export default function App() {
+  const [user, setUser] = React.useState(null);
+  React.useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      setUser(user);
+    });
+  }, [user]);
+
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
+        {/* <Route path="/signup" element={<SignUp />}></Route> */}
+        <Route
+          path="/dashboard"
+          element={<Dashboard isAuthed={!!user} />}
+        ></Route>
+        <Route path="/addRestaurant" element={<AddRestaurant />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
       </Routes>
     </>
   );
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,7 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { Result } from "postcss";
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_APIKEY,
@@ -10,12 +14,10 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
   appId: import.meta.env.VITE_APP_APPID,
-  measurementId: import.meta.env.VITE_APP_MEASUREMENTID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
@@ -24,8 +26,21 @@ export const signInWithGoogle = function () {
   signInWithPopup(auth, provider)
     .then((res) => {
       console.log(res);
+      localStorage.setItem("token", res._tokenResponse.idToken);
+      localStorage.setItem("uid", res.user.uid);
     })
     .catch((error) => {
       console.error(error);
+    });
+};
+
+export const signOutNow = function () {
+  signOut(auth)
+    .then(() => {
+      localStorage.clear();
+      console.log("signed out");
+    })
+    .catch((err) => {
+      console.error(err);
     });
 };
