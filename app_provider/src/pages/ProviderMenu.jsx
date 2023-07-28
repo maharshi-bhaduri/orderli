a78260f28b04e382d484401864238074e52dd622
xav1994das <xav0anime@gmail.com>
2023-07-28 18:40:25 +0530

    feedback card add

--- app_provider/src/pages/ProviderMenu.jsx
+++ app_provider/src/pages/ProviderMenu.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
-import { useMutation, useQueryClient } from 'react-query';
+import { useMutation, useQueryClient } from "react-query";
 import { postService } from "../utils/APIService";
 import { getMenu } from "../utils/queryService";
 import localforage from "localforage";
@@ -10,16 +10,16 @@ export default function ProviderMenu() {
   const location = useLocation();
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-  const [newAdditionDisabled, setNewAdditionDisabled] = useState(false)
+  const [newAdditionDisabled, setNewAdditionDisabled] = useState(false);
   const { providerHandle } = useParams();
   const { data: menu, isLoading, isError } = getMenu(providerHandle);
   const [updatedMenu, setUpdatedMenu] = useState([]);
   const defaultNewMenuItem = {
-    menuId: Date.now(),
+    menuId: Date.now(), //temporary
     itemName: "",
     description: "",
-    price: 0
-  }
+    price: 0,
+  };
 
   const [newMenuItem, setNewMenuItem] = useState(defaultNewMenuItem);
 
@@ -29,9 +29,9 @@ export default function ProviderMenu() {
   const [editedMenuItem, setEditedMenuItem] = useState({});
 
   const updateMenuItemsWithCachedOperations = async () => {
-    const addOperations = await localforage.getItem('add') || [];
-    const updateOperations = await localforage.getItem('update') || [];
-    const deleteOperations = await localforage.getItem('delete') || [];
+    const addOperations = (await localforage.getItem("add")) || [];
+    const updateOperations = (await localforage.getItem("update")) || [];
+    const deleteOperations = (await localforage.getItem("delete")) || [];
 
     const updatedMenuItems = menu.map((menuItem) => {
       const updateOperation = updateOperations.find(
@@ -41,18 +41,19 @@ export default function ProviderMenu() {
         (operation) => operation.menuId === menuItem.menuId
       );
       if (updateOperation) {
-        return { ...updateOperation, operation: 'update' };
+        return { ...updateOperation, operation: "update" };
       }
       if (deleteOperation) {
-        return { ...deleteOperation, operation: 'delete' };
+        return { ...deleteOperation, operation: "delete" };
       }
       return { ...menuItem, operation: null };
     });
-    const removedDeletes = updatedMenuItems.filter((item) => item.operation !== 'delete');
-    const updatedMenuWithAdditions = [...removedDeletes,
-    ...addOperations.map(
-      (menuItem) => ({ ...menuItem, operation: 'add' })
-    )
+    const removedDeletes = updatedMenuItems.filter(
+      (item) => item.operation !== "delete"
+    );
+    const updatedMenuWithAdditions = [
+      ...removedDeletes,
+      ...addOperations.map((menuItem) => ({ ...menuItem, operation: "add" })),
     ];
     setUpdatedMenu(updatedMenuWithAdditions);
   };
@@ -68,26 +69,26 @@ export default function ProviderMenu() {
       postService(import.meta.env.VITE_APP_UPDATE_MENU, cachedChanges),
     {
       onSuccess: () => {
-        queryClient.invalidateQueries(['menu', providerHandle]);
-      }
+        queryClient.invalidateQueries(["menu", providerHandle]);
+      },
     }
   );
 
   const handleSaveMenu = async () => {
-    const addOperations = await localforage.getItem('add') || [];
-    const updateOperations = await localforage.getItem('update') || [];
-    const deleteOperations = await localforage.getItem('delete') || [];
+    const addOperations = (await localforage.getItem("add")) || [];
+    const updateOperations = (await localforage.getItem("update")) || [];
+    const deleteOperations = (await localforage.getItem("delete")) || [];
 
     const removeOperationKey = (obj) => {
       const { operation, ...updatedData } = obj;
       return updatedData;
     };
-    const addListWithoutOperation = addOperations.map(removeOperationKey).map(
-      (obj) => {
+    const addListWithoutOperation = addOperations
+      .map(removeOperationKey)
+      .map((obj) => {
         const { menuId, ...updatedData } = obj;
         return updatedData;
-      }
-    );
+      });
     const updateListWithoutOperation = updateOperations.map(removeOperationKey);
     const deleteListWithoutOperation = deleteOperations.map(removeOperationKey);
 
@@ -95,40 +96,42 @@ export default function ProviderMenu() {
       providerHandle: providerHandle,
       addMenuItems: addListWithoutOperation,
       updateMenuItems: updateListWithoutOperation,
-      deleteMenuItems: deleteListWithoutOperation
-    }
+      deleteMenuItems: deleteListWithoutOperation,
+    };
 
-    await localforage.setItem('update', []);
-    await localforage.setItem('add', []);
-    await localforage.setItem('delete', []);
+    await localforage.setItem("update", []);
+    await localforage.setItem("add", []);
+    await localforage.setItem("delete", []);
 
-    updateMenu(cachedChanges)
-  }
+    updateMenu(cachedChanges);
+  };
 
   const handleAddMenuItem = async () => {
-    const addOperations = await localforage.getItem('add') || [];
+    const addOperations = (await localforage.getItem("add")) || [];
     // setNewAdditionDisabled(true);
     // const newItemOper = { ...newMenuItem, operation: "add" }
     addOperations.push(newMenuItem);
     // addMenuItem(newMenuItem);
-    setUpdatedMenu((prevUpdatedMenu) => [...prevUpdatedMenu, { ...newMenuItem, operation: "add" }])
-    await localforage.setItem('add', addOperations);
+    setUpdatedMenu((prevUpdatedMenu) => [
+      ...prevUpdatedMenu,
+      { ...newMenuItem, operation: "add" },
+    ]);
+    await localforage.setItem("add", addOperations);
 
     setNewMenuItem(defaultNewMenuItem);
   };
 
   const handleUpdateMenuItem = async (menuItem) => {
-    console.log("menuItem ", menuItem)
-    if (menuItem.operation === 'add') {
-      const addOperations = await localforage.getItem('add');
+    console.log("menuItem ", menuItem);
+    if (menuItem.operation === "add") {
+      const addOperations = await localforage.getItem("add");
       const updatedAddCache = addOperations.map((cachedItem) =>
         cachedItem.menuId === menuItem.menuId ? menuItem : cachedItem
       );
-      await localforage.setItem('add', updatedAddCache);
-    }
-    else if (!menuItem.operation || menuItem.operation === "update") {
-      const updateOperations = await localforage.getItem('update') || [];
-      const updatedOperations = updateOperations.map(operation => {
+      await localforage.setItem("add", updatedAddCache);
+    } else if (!menuItem.operation || menuItem.operation === "update") {
+      const updateOperations = (await localforage.getItem("update")) || [];
+      const updatedOperations = updateOperations.map((operation) => {
         if (operation.menuId === menuItem.menuId) {
           return menuItem;
         }
@@ -141,27 +144,28 @@ export default function ProviderMenu() {
       if (!isMenuItemUpdated) {
         updatedOperations.push(menuItem);
       }
-      await localforage.setItem('update', updatedOperations);
+      await localforage.setItem("update", updatedOperations);
     }
     updateMenuItemsWithCachedOperations();
     setIsEditing(false);
   };
 
   const handleDeleteMenuItem = async (menuItem) => {
-    if (menuItem.operation === 'add') {
-      const addOperations = await localforage.getItem('add');
-      const indexToRemove = addOperations.findIndex((item) => item.menuId === menuItem.menuId);
+    if (menuItem.operation === "add") {
+      const addOperations = await localforage.getItem("add");
+      const indexToRemove = addOperations.findIndex(
+        (item) => item.menuId === menuItem.menuId
+      );
       // If the menu item is found, remove it from the array
       if (indexToRemove !== -1) {
         addOperations.splice(indexToRemove, 1);
         // Save the updated addOperations array back to localforage
-        await localforage.setItem('add', addOperations);
+        await localforage.setItem("add", addOperations);
       }
-    }
-    else if (menuItem.operation === null || menuItem.operation === "update") {
-      const deleteOperations = await localforage.getItem('delete') || [];
+    } else if (menuItem.operation === null || menuItem.operation === "update") {
+      const deleteOperations = (await localforage.getItem("delete")) || [];
       deleteOperations.push(menuItem);
-      await localforage.setItem('delete', deleteOperations);
+      await localforage.setItem("delete", deleteOperations);
       setDeleteRow(menuItem.menuId);
       // deleteMenuItem(menuItem);
     }
@@ -171,17 +175,17 @@ export default function ProviderMenu() {
   const handleEditMenuItem = (menuItem) => {
     setEditItemId(menuItem.menuId);
     setIsEditing(true);
-    setEditedMenuItem(menuItem)
+    setEditedMenuItem(menuItem);
   };
 
   const cancelEditMenuItem = async (editedMenuItem) => {
-    const updateOperations = await localforage.getItem('update') || [];
+    const updateOperations = (await localforage.getItem("update")) || [];
 
     const updatedOperations = updateOperations.filter(
       (operation) => operation.menuId !== editedMenuItem.menuId
     );
 
-    await localforage.setItem('update', updatedOperations);
+    await localforage.setItem("update", updatedOperations);
     setIsEditing(false);
   };
 
@@ -195,7 +199,7 @@ export default function ProviderMenu() {
               type="add"
               handleAddMenuItem={() => handleAddMenuItem()}
               onChange={(e) => {
-                setNewMenuItem(e)
+                setNewMenuItem(e);
               }}
             />
           </tbody>
@@ -206,60 +210,65 @@ export default function ProviderMenu() {
           <p>Loading menu items...</p>
         ) : isError ? (
           <p>Error loading menu items.</p>
-        ) : updatedMenu?.length == 0 ? <div className="flex w-3/4 justify-center rounded-lg border border-gray-300 bg-gray-100 p-5">
-          <h1>Ready to Set the Table? Add an Item!</h1>
-        </div>
-          :
-          (
-            <table className="w-3/4 mt-4">
-              <thead>
-                <tr>
-                  <th className="py-2 px-4 bg-gray-100 text-left">Item Name</th>
-                  <th className="py-2 px-4 bg-gray-100 text-left">Description</th>
-                  <th className="py-2 px-4 bg-gray-100 text-left">Price</th>
-                  <th className="py-2 px-4 bg-gray-100 text-left">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {!isLoading &&
-                  updatedMenu.map((menuItem) => (
-                    editItemId === menuItem.menuId && isEditing ? (
-                      <MenuEditRow
-                        key={menuItem.menuId}
-                        editedMenuItem={editedMenuItem}
-                        cancelEditMenuItem={() => cancelEditMenuItem(editedMenuItem)}
-                        handleUpdateMenuItem={() => handleUpdateMenuItem(editedMenuItem)}
-                        type="update"
-                        onChange={(e) => {
-                          setEditedMenuItem(e);
-                        }}
-                      />
-                    ) : (
-                      <tr key={menuItem.menuId}>
-                        <td className="py-2 px-4 w-1/4">{menuItem.itemName}</td>
-                        <td className="py-2 px-4 w-1/4">{menuItem.description}</td>
-                        <td className="py-2 px-4 w-1/4">{menuItem.price}</td>
-                        <td className="py-2 px-4 w-1/4">
-                          <button
-                            onClick={() => handleEditMenuItem(menuItem)}
-                            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 mx-2"
-                          >
-                            Edit
-                          </button>
-                          <button
-                            onClick={() => handleDeleteMenuItem(menuItem)}
-                            className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 mx-2"
-                          >
-                            Delete
-                          </button>
-                        </td>
-                      </tr>
-                    )
-                  ))}
-              </tbody>
-            </table>
-          )
-        }
+        ) : updatedMenu?.length == 0 ? (
+          <div className="flex w-3/4 justify-center rounded-lg border border-gray-300 bg-gray-100 p-5">
+            <h1>Ready to Set the Table? Add an Item!</h1>
+          </div>
+        ) : (
+          <table className="w-3/4 mt-4">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 bg-gray-100 text-left">Item Name</th>
+                <th className="py-2 px-4 bg-gray-100 text-left">Description</th>
+                <th className="py-2 px-4 bg-gray-100 text-left">Price</th>
+                <th className="py-2 px-4 bg-gray-100 text-left">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {!isLoading &&
+                updatedMenu.map((menuItem) =>
+                  editItemId === menuItem.menuId && isEditing ? (
+                    <MenuEditRow
+                      key={menuItem.menuId}
+                      editedMenuItem={editedMenuItem}
+                      cancelEditMenuItem={() =>
+                        cancelEditMenuItem(editedMenuItem)
+                      }
+                      handleUpdateMenuItem={() =>
+                        handleUpdateMenuItem(editedMenuItem)
+                      }
+                      type="update"
+                      onChange={(e) => {
+                        setEditedMenuItem(e);
+                      }}
+                    />
+                  ) : (
+                    <tr key={menuItem.menuId}>
+                      <td className="py-2 px-4 w-1/4">{menuItem.itemName}</td>
+                      <td className="py-2 px-4 w-1/4">
+                        {menuItem.description}
+                      </td>
+                      <td className="py-2 px-4 w-1/4">{menuItem.price}</td>
+                      <td className="py-2 px-4 w-1/4">
+                        <button
+                          onClick={() => handleEditMenuItem(menuItem)}
+                          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 mx-2"
+                        >
+                          Edit
+                        </button>
+                        <button
+                          onClick={() => handleDeleteMenuItem(menuItem)}
+                          className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 mx-2"
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  )
+                )}
+            </tbody>
+          </table>
+        )}
       </div>
       <button
         onClick={() => handleSaveMenu()}
@@ -270,5 +279,4 @@ export default function ProviderMenu() {
       </button>
     </div>
   );
-
 }
