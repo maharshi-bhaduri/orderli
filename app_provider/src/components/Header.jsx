fdec9e8110ced3821db04bd28a7a00e004d03f28
Maharshi Bhaduri <bhaduri.maharshi@gmail.com>
2023-07-28 21:20:32 +0530

    updated feedback pallete

--- app_provider/src/components/Header.jsx
+++ app_provider/src/components/Header.jsx
@@ -5,20 +5,13 @@ import { useNavigate } from "react-router-dom";
 export default function Header(props) {
   const navigate = useNavigate();
   return (
-    <header className="fixed flex justify-between align-center items-center h-20 w-full py-2 px-5 top-0">
+    <header className="fixed flex justify-between align-center items-center h-16 w-full py-2 px-5 top-0">
       <div className="rounded-full p-4 select-none cursor-pointer">
         <h1 className="text-2xl">orderlee.in</h1>
       </div>
-      <div className="rounded-full bg-orange-400 p-2">
+      <div className="rounded-full p-2">
         <button
-          className="rounded-full"
-          style={{
-            backgroundColor: "#fa6400",
-            color: "white",
-            padding: "10px",
-            border: "1px solid transparent",
-            cursor: "pointer",
-          }}
+          className="rounded-full bg-orange-400 text-white p-3"
           onClick={() => navigate("/login")}
         >
           Sign In
--- app_provider/src/pages/ProviderFeedback.jsx
+++ app_provider/src/pages/ProviderFeedback.jsx
@@ -7,9 +7,15 @@ export default function ProviderFeedback() {
   const { data: feedback, isLoading, isError } = getFeedback(providerHandle);
 
   return (
-    <>
-      <div className="max-w-5xl mx-auto mt-12">
-        <header className="text-4xl">Customer Feedback</header>
+    <div className="w-full px-8 flex flex-col items-center">
+      <div className="w-full my-6">
+        <header className="text-4xl font-medium">Customer Feedback</header>
+      </div>
+      <div
+        className={"rounded-lg bg-white " +
+          "w-full p-5 transition ease-in-out flex flex-col justify-around items-center"}
+      >
+        {/* </div><div className="max-w-5xl mx-auto mt-12"> */}
         {!isLoading &&
           feedback.map((feedbackitem, index) => (
             <div key={index}>
@@ -21,6 +27,6 @@ export default function ProviderFeedback() {
             </div>
           ))}
       </div>
-    </>
+    </div>
   );
 }
