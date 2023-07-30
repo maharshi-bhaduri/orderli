00351b0d106bec3363c5e0541e67db9391fbb2eb
Maharshi Bhaduri <bhaduri.maharshi@gmail.com>
2023-07-31 00:29:20 +0530

    temp patch for grid issue

--- app_provider/src/components/InfoGrid.jsx
+++ app_provider/src/components/InfoGrid.jsx
@@ -12,7 +12,7 @@ export default function InfoGrid(props) {
             <div className="px-2 text-sm font-bold text-gray-600">
                 {props.title}
             </div>
-            <div className={`my-4 grid gap-x-4 gap-y-8 w-full grid-cols-${props.cols}`} >
+            <div className={`my-4 grid gap-x-4 gap-y-8 w-full grid-cols-2 ${props.cols}`} >
                 {
                     props.infoList.map((item, index) =>
                         <div
