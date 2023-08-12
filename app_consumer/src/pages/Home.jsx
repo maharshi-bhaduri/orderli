4bee222b1ff7e1f6fbdf36fdff3c309f957c1abc
Maharshi Bhaduri <bhaduri.maharshi@gmail.com>
2023-08-13 02:27:47 +0530

    updated home page

--- app_consumer/src/pages/Home.jsx
+++ app_consumer/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { getMenu } from "../utils/queryService";
 import Header from "../components/Header";
+import bg from "../images/cafe_bg.jpg"
 export default function Home() {
   const navigate = useNavigate();
   let { providerHandle } = useParams();
@@ -13,13 +14,22 @@ export default function Home() {
   }
   return (
     <>
-      <Header />
-      <div className="flex h-screen justify-center items-center bg-cover bg-center bg-multiply bg-opacity-10">
+      {/* <Header /> */}
+      <div className="fixed -z-10 bg-stone-600 opacity-50">
+        <img src={bg}
+          className="object-cover h-screen"
+        />
+      </div>
+      <div className="flex flex-col h-screen justify-center items-center mx-4">
+        <h1 className="text-4xl mb-5 shadow-inner">
+          Welcome to Cafe Magenta
+        </h1>
         <button
           onClick={goToMenu}
-          className="order-btn bg-orderlee-primary-100 hover:bg-orange-500  text-white font-bold py-5 px-10 rounded-full"
+          className="mt-10 order-btn bg-black hover:bg-warmGray-800 text-white
+          font-bold py-5 px-10 rounded-full shadow-lg transition-all duration-300 ease-in-out"
         >
-          ORDER NOW
+          Menu
         </button>
       </div>
     </>
