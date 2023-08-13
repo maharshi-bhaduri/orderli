fe1696e604c69092ecb3c6083292f5bccd633e27
Maharshi Bhaduri <bhaduri.maharshi@gmail.com>
2023-08-13 14:40:23 +0530

    styled home button

--- app_consumer/src/components/TabOption.jsx
+++ app_consumer/src/components/TabOption.jsx
@@ -5,7 +5,7 @@ export default function TabOption(props) {
   return (
     <div
       className={
-        "inline rounded-lg border border-gray-300 m-2 p-2 transition ease-in-out cursor-pointer select-none hover:bg-gray-300 " +
+        "inline rounded-lg text-gray-500 border border-gray-300 mx-2 p-2 w-[100px] transition ease-in-out cursor-pointer select-none hover:bg-gray-300 " +
         (props.active
           ? (" cursor-default " +
             (props.name === "Veg" ? "bg-green-50 border-green-700 "
