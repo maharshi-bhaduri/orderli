d3d2efee8aaa375bdcc66f367764591d57c960f8
xav1994das <xav0anime@gmail.com>
2024-12-03 22:42:39 +0530

    table

--- app_provider/src/components/TableSquare.jsx
+++ app_provider/src/components/TableSquare.jsx
@@ -2,15 +2,36 @@ export default function TableSquare({ table, onClick }) {
   const { tableId, seatingCapacity, status } = table;
 
   // Determine color based on status
-  const bgColor = status === "Available" ? "bg-green-500" : "bg-red-500";
+  const bgColor =
+    status === "Available"
+      ? "bg-green-300"
+      : status === "Reserved"
+      ? "bg-yellow-300"
+      : "bg-red-300"; // Occupied
+
+  const textColor =
+    status === "Available"
+      ? "text-green-800"
+      : status === "Reserved"
+      ? "text-yellow-800"
+      : "text-red-800"; // Occupied
 
   return (
     <div
-      className={`h-16 flex flex-col items-center justify-center 
-                    rounded-md text-white font-medium ${bgColor} cursor-pointer`}
+      className={`h-20 w-40 flex flex-col relative rounded-xl 
+                  text-black font-medium overflow-hidden ${bgColor} cursor-pointer`}
       onClick={onClick}
     >
-      <div>{`Table #${tableId}`}</div>
+      {/* Main Content Area */}
+      <div className="flex-1 w-full flex flex-col items-center justify-center ">
+        <div
+          className={`text-sm ${textColor} font-semibold`}
+        >{`Table ${tableId}`}</div>
+        <div className={`flex ${textColor}`}>
+          <span className="material-symbols-outlined text-sm">Group</span>
+          <span className="pl-2 text-sm">{seatingCapacity}</span>
+        </div>
+      </div>
     </div>
   );
 }
